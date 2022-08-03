666c91ebb457e07739284bc3e344030bccde1efb
Beto Toro <beto.toro@zemoga.com>
2022-08-03 10:04:10 -0500

    Initial Commit

---
+++ Users/UsersApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsersApp.swift
+//  Users
+//
+//  Created by Beto Toro on 3/08/22.
+//
+
+import SwiftUI
+
+@main
+struct UsersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
